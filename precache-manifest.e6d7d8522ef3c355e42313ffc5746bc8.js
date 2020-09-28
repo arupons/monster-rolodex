8b0c94d25ae1e14834434a5fb9a9050067cdbc0b
Alfonso Aguilar <aruponse@gmail.com>
2020-09-28 00:23:33 -0500

    Updates

---
+++ precache-manifest.e6d7d8522ef3c355e42313ffc5746bc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057f3640c8f693dcf851709824e8cead",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "544d392eb6745a4c8eae",
+    "url": "/monster-rolodex/static/css/main.bbf3b27e.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "544d392eb6745a4c8eae",
+    "url": "/monster-rolodex/static/js/main.be000d19.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
